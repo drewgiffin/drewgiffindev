959209edf075e2524327bf8219e5dc09a12b2d2b
Drew Giffin <drewgiffin97@gmail.com>
2026-03-07 15:25:25 -0500

    Moved secret key from base setting to environment file

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,7 +1,10 @@
 from .base import *
+load_dotenv(f"{BASE_DIR}/.env.development")
 
 DEBUG = True
 ALLOWED_HOSTS = [
     "localhost", 
     "127.0.0.1"
     ]
+
+SECRET_KEY = os.getenv("SECRET_KEY")
